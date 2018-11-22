1418c00b489a4860f8678e874e357a0077e62141
David Straub <david.straub@tum.de>
2018-11-22 11:26:34 +0100

    Revert "Move script, set environment variables"
    This reverts commit fedbf6f4b4c2dc5f702f98abf12d11135e75e6a1.

--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ import os
 from distutils.core import setup
 from distutils.extension import Extension
 
-
 incdir_src = os.path.abspath("LHAPDF-6.2.1/include")
 incdir_build = os.path.abspath("LHAPDF-6.2.1/include")
 libdir = os.path.abspath("LHAPDF-6.2.1/src/.libs")
 
-
-# Configure the C++ extension and LHAPDF package
-ext = Extension("_lhapdf",
+## Configure the C++ extension and LHAPDF package
+ext = Extension("lhapdf",
                 ["LHAPDF-6.2.1/wrappers/python/lhapdf.cpp"],
                 include_dirs=[incdir_src, incdir_build],
                 extra_compile_args=["-I/home/travis/build/include"],
@@ -21,13 +19,6 @@ ext = Extension("_lhapdf",
                 scripts=['bin/lhapdf'],
                 )
 
-
 setup(name = "LHAPDF",
-      version = "6.2.1-4",
-      py_modules=['lhapdf'],
-      ext_modules = [ext],
-      package_data={
-        'lhapdf': ['lhapdf.conf']
-      },
-      install_requires=['setuptools', 'appdirs'],
-)
+      version = "6.2.1-3",
+      ext_modules = [ext])
